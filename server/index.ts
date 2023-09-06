2cd40ed30754284c7bb4a482e35a67d84aa15000
Grellheist <grellheist@tutanota.com>
2023-09-06 09:39:05 -0300

    changed to typescript

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express from "express";
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+export default server;
